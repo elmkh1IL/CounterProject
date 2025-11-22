516fa65019ec5e68f5811fe7c50909ad542aff83
el.mkh <el.mkh@inbox.ru>
2025-11-22 18:11:47 +0300

    Создан проект

---
+++ CounterProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterProject
+//
+//  Created by xxx on 22.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
